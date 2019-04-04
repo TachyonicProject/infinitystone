5583e2caaed16686ccc3c3d6c55c92f0bab3a0fe
Christiaan Frans Rademan <christiaan.rademan@gmail.com>
2019-04-04 10:07:16 +0200

    fixes/updates

--- infinitystone/helpers/tenants.py
+++ infinitystone/helpers/tenants.py
@@ -29,7 +29,6 @@
 # THE POSSIBILITY OF SUCH DAMAGE.
 
 from luxon import db
-from luxon.utils.sql import build_where, build_like
 
 
 def get_tenant_domain(tenant_id):
@@ -41,6 +40,7 @@ def get_tenant_domain(tenant_id):
                               tenant_id).fetchone()
         if result:
             return result['domain']
+
 
 def get_sub_tenants(tenant_id):
     tenants = []
@@ -70,79 +70,3 @@ def tenant_or_sub(tenant_id):
             return result['tenant_id']
         elif result and not result['tenant_id']:
             return tenant_id
-
-
-def get_tenants(user_id=None, domain=None, tenant_id=None,
-                page=1, limit=10, search=None):
-    start = (page - 1) * limit
-    sql = "SELECT DISTINCT"
-    sql += " infinitystone_tenant.id as id,"
-    sql += " infinitystone_tenant.name as name,"
-    sql += " infinitystone_tenant.crm_id as crm_id,"
-    sql += " infinitystone_tenant.domain as domain,"
-    sql += " infinitystone_tenant.tenant_id as tenant_id"
-    sql += " FROM infinitystone_tenant"
-    sql += " INNER JOIN infinitystone_user_role"
-    sql += " ON"
-    # Check if domain and tenant matches.
-    sql += " (infinitystone_user_role.tenant_id ="
-    sql += " infinitystone_tenant.id"
-    sql += " AND infinitystone_user_role.domain ="
-    sql += " infinitystone_tenant.domain)"
-    # Check if domain and sub tenant matches.
-    sql += " OR (infinitystone_user_role.tenant_id ="
-    sql += " infinitystone_tenant.tenant_id"
-    sql += " AND infinitystone_user_role.domain ="
-    sql += " infinitystone_tenant.domain)"
-    # Check if tenant is null and domain matches.
-    sql += " OR (infinitystone_user_role.tenant_id is null"
-    sql += " AND infinitystone_user_role.domain ="
-    sql += " infinitystone_tenant.domain)"
-    # Check if tnenat is null and domain is null
-    sql += " OR (infinitystone_user_role.tenant_id is null"
-    sql += " AND infinitystone_user_role.domain is null)"
-    # Check if domain is null and tenant id matches.
-    sql += " OR (infinitystone_user_role.domain is null"
-    sql += " AND infinitystone_user_role.tenant_id ="
-    sql += " infinitystone_tenant.id)"
-    # Check if domain is null and sub tenant id matches.
-    sql += " OR (infinitystone_user_role.domain is null"
-    sql += " AND infinitystone_user_role.tenant_id ="
-    sql += " infinitystone_tenant.tenant_id)"
-
-    where = {}
-    where2 = {}
-    like = {}
-    if user_id:
-        where['infinitystone_user_role.user_id'] = user_id
-    if domain:
-        if domain.lower() == 'none':
-            where['infinitystone_tenant.domain'] = None
-        else:
-            where['infinitystone_tenant.domain'] = domain
-    if tenant_id:
-        where2 = {}
-        where2['infinitystone_tenant.tenant_id'] = tenant_id
-        where2['infinitystone_tenant.id'] = tenant_id
-
-    where, values = build_where(**where)
-    where2, values2 = build_where('OR', **where2)
-    if values2:
-        where += " AND " + where2
-        values += values2
-
-    if search:
-        where3, values3 = build_like('OR', **search)
-        if values3:
-            where += " AND " + where3
-            values += values3
-
-    with db() as conn:
-        if values:
-            return conn.execute(sql + 'WHERE ' + where +
-                                ' LIMIT %s, %s' % (start, limit + 100,),
-                                values).fetchall()
-        else:
-            return conn.execute(sql +
-                                ' LIMIT %s, %s' % (start, limit + 100,),
-                                values).fetchall()
