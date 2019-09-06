126b29905733e24ddc6c11d40327b1671790e562
Christiaan Frans Rademan <christiaan.rademan@gmail.com>
2019-09-06 16:07:40 +0200

    metadata for tokens added

--- infinitystone/auth/keystone.py
+++ infinitystone/auth/keystone.py
@@ -30,6 +30,7 @@
 from luxon import g
 from psychokinetic.openstack import Openstack
 
+
 class Keystone(object):
     def password(self, username, domain, credentials):
 
@@ -52,6 +53,7 @@ class Keystone(object):
             raise ValueError("No 'domain' provided")
 
         os.identity.authenticate(username, password, domain)
-        g.current_request.context['openstack'] = os
 
-        return True
+        return {
+            'openstack_token': os._login_token
+        }
